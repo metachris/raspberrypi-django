a8434d1376ebeb13466e8fda9f84b22a49ec74da
Chris Hager <chris@metachris.org>
2013-01-24 03:27:49 +0100

    really nice gpio daemon start

--- gpiodaemon/gpiodaemon.py
+++ gpiodaemon/gpiodaemon.py
@@ -1,11 +1,27 @@
+#!/usr/bin/env python
+import sys
+import signal
+import socket
 import traceback
+from os import getpid, remove, kill
+from optparse import OptionParser
 
 from tornado.ioloop import IOLoop
 from tornado.netutil import TCPServer
 
 import gpiomanager
 
+PORT = 9101
+PIDFILE = "/tmp/gpiodaemon.pid"
 
+# We shut the daemon down by sending the SIGINT signal to this process
+# (eg $ kill -s SIGINT [proc-id])
+def signal_handler(signal, frame):
+    sys.exit(0)
+signal.signal(signal.SIGINT, signal_handler)
+
+
+# Each connected client gets a TCPConnection object
 class TCPConnection(object):
     def __init__(self, gpio, stream, address):
         print '- new connection from %s' % repr(address)
@@ -21,6 +37,7 @@ class TCPConnection(object):
             self.stream.close()
             return
 
+
         print '- from %s: "%s"' % (repr(self.address), data)
         self.GPIO.handle_cmd(data)
         self.stream.read_until('\n', self._on_read_line)
@@ -32,6 +49,7 @@ class TCPConnection(object):
         print '- client quit %s' % repr(self.address)
 
 
+# The one main server
 class GPIOServer(TCPServer):
     def __init__(self, gpio, io_loop=None, ssl_options=None, **kwargs):
         self.GPIO = gpio
@@ -42,18 +60,59 @@ class GPIOServer(TCPServer):
         TCPConnection(self.GPIO, stream, address)
 
 
+# Setup & Startup
 def main():
+    with open(PIDFILE, "w") as f:
+        f.write("%s" % getpid())
+
     GPIO = gpiomanager.GPIO()
     chat_server = GPIOServer(GPIO)
-    chat_server.listen(9101)
+    chat_server.listen(PORT)
 
     try:
         IOLoop.instance().start()
+    except SystemExit:
+        print "Shutting down via signal"
     except:
         print traceback.format_exc()
     finally:
         GPIO.cleanup()
         print "GPIOServer stopped"
 
+    remove(PIDFILE)
+
+
+def daemon_shutdown():
+    """Shuts down a daemon running on localhost by sending SIGINT"""
+    pid = open(PIDFILE).read()
+    kill(int(pid), signal.SIGINT)
+
+
+def daemon_reload():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        sock.connect(("localhost", PORT))
+        sock.sendall("reload\n\n")  # second newline (empty packet) terminates the connection server-side
+    finally:
+        sock.close()
+
+
 if __name__ == '__main__':
-    main()
+    usage = """usage: %prog [options]"""
+    desc="""GPIO-Daemon is a socket interface for controlling the GPIO ports on the
+    Raspberry pi. It listens on port %s for TCP connections and can execute commands.""" % PORT
+    parser = OptionParser(usage=usage, description=desc)
+
+    parser.add_option("--stop", default=False,
+        action="store_true", dest="stop", help="stops a running gpio daemon")
+
+    parser.add_option("--reload", default=False,
+        action="store_true", dest="reload", help="reload the configuration and reset gpio")
+
+    (options, args) = parser.parse_args()
+    if options.stop:
+        daemon_shutdown()
+    elif options.reload:
+        daemon_reload()
+    else:
+        main()
--- gpiodaemon/gpiomanager.py
+++ gpiodaemon/gpiomanager.py
@@ -1,9 +1,13 @@
 """
 Manages communication with GPIO and receiving commands
 
-RPi.GPIO works with pin-ids instead of GPIO-ids (for instance GPIO17 has pin-id 11)
+RPi.GPIO works with pin-ids instead of GPIO-ids (for instance GPIO17 has pin-id 11).
+
+GPIO-Manager can also asynchronously run commands and replace existing timeouts on them with new ones.
 """
 import yaml
+import time
+from threading import Thread
 
 # Import either the dummy or the real lib
 try:
@@ -21,6 +25,23 @@ GPIO_TO_PIN_MAP = {
 }
 
 
+class AsyncCmd(Thread):
+    is_cancelled = False
+    def __init__(self, timeout_sec, cmd, handle_cmd_cb, is_replaceable=True):
+        # If is_replaceable is True and another timeout with the same command is added, the
+        # existing timeout will be suspended and only the new one executed.
+        Thread.__init__(self)
+        self.timeout_sec = timeout_sec
+        self.cmd = cmd
+        self.handle_cmd_cb = handle_cmd_cb  # callback to execute command with
+        self.is_replaceable = is_replaceable
+
+    def run(self):
+        time.sleep(self.timeout_sec)
+        if not self.is_cancelled:
+            self.handle_cmd_cb(self.cmd)
+
+
 class GPIO(object):
     INPUT = RPiGPIO.IN
     OUTPUT = RPiGPIO.OUT
@@ -29,8 +50,10 @@ class GPIO(object):
 
     config = None
     commands = None
+    async_pool = []
 
     def __init__(self):
+        RPiGPIO.setmode(RPiGPIO.BOARD)
         self._gpio_init()
 
     # Public Functions
@@ -54,9 +77,10 @@ class GPIO(object):
         if cmd == "reload":
             self.reload()
         elif cmd in self.commands:
+            # translate user-command to system-command and execute
             self._handle_cmd(self.commands[cmd])
         else:
-            print "- not found"
+            self._handle_cmd(cmd)
 
     # Internal Functions
     def _gpioid_to_pinid(self, gpio_id):
@@ -70,7 +94,6 @@ class GPIO(object):
 
     def _gpio_init(self):
         # Read config and set modes accordingly
-        RPiGPIO.setmode(RPiGPIO.BOARD)
         RPiGPIO.cleanup()
 
         self._reload_config()
@@ -91,8 +114,10 @@ class GPIO(object):
     def _handle_cmd(self, internal_cmd):
         # Internal cmd is the actuall command (triggered by the user command)
         print "execute> %s" % internal_cmd
-        cmd, gpio_id, value = internal_cmd.split(" ")[:3]
+        cmd_parts = internal_cmd.split(" ")
+        cmd = cmd_parts[0]
         if cmd == "set":
+            gpio_id, value = cmd_parts[1:3]
             if value == "HIGH":
                 value = self.HIGH
             elif value == "LOW":
@@ -100,12 +125,33 @@ class GPIO(object):
             else:
                 print "Error cannot handle command '%s' due to bad value" % internal_cmd
                 return
-
             self.gpio_output(int(gpio_id), value)
 
+        elif cmd == "rtimeout":
+            # Replaceable timeout. Replaces based on "cmd" only.
+            timeout = cmd_parts[1]
+            cmd = " ".join(cmd_parts[2:])
+            print "rtimeout: %s: %s" % (timeout, cmd)
+
+            # Disable all old ones from the pool
+            for async_cmd in self.async_pool:
+                if async_cmd.cmd == cmd and async_cmd.is_replaceable:
+                    async_cmd.is_cancelled = True
+
+            # Now add new task
+            t = AsyncCmd(int(timeout), cmd, self.handle_cmd, is_replaceable=True)
+            t.start()
+            self.async_pool.append(t)
+
+        else:
+            print "command not recognized"
 
 
 if __name__ == "__main__":
+    # Some testing
     g = GPIO()
     g.handle_cmd("thermo on")
+    g.handle_cmd("rtimeout 3 thermo off")
+
+    time.sleep(5)
     g.cleanup()
